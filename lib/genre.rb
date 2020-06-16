0b1d97a59c6b2a614ce9bc91f06bd955ddf1ac20
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-16 14:55:15 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -4,12 +4,20 @@ class Genre
 
   @@all = []
 
+  def initialize(name)
+    @name = name
+  end
+
   def songs
     Song.all.select {|song| song.genre == self}
   end
 
   def self.all
     @@all
+  end
+
+  def artists
+    self.songs.collect {|song| song.genre}
   end
 
 end
