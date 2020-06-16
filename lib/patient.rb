5e037ba5254bf3c62f593e9680b78c77807c243f
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-16 15:22:19 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,4 +17,8 @@ class Patient
   def appointments
     Appointment.all {|appointment| appointment.patient == self}
   end
+
+  def doctors
+    self.appointments.collect {|appointment| appointment.doctor}
+  end
 end
